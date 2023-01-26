1e909668291b3976af3127798656fb4394a2c864
mithileshpatel <mithileshpatel250@gmail.com>
2023-01-26 17:18:40 +0530

    types_Of_Fuctions created and modified

---
+++ types_Of_Fuctions.js
@@ -0,0 +1,42 @@
+//Function Statement  AKA Function Declaration
+
+function a() {
+    console.log("a called");
+}
+
+//Function Expression
+
+var b= function() {
+    console.log("b called");
+}
+
+
+// Anonymous function
+
+    // Function withour a name
+    // function (){
+
+    // }
+
+
+//Named Function expression
+
+var c = function xyz() {
+    console.log("b called");
+}
+
+
+
+//Difference btween Parameter and Arguments
+    // Function declaration---> Parameter
+    // Function calling---> Arguments
+
+
+
+
+//First class functions AKA First class citizens 
+    // The ability to use functions as a value is knows a first class functons
+
+
+
+//Arrow functions
